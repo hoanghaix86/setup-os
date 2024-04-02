8715d7b4d75094e84c2baf59deb3f0353444cfe2
hoanghaix86 <97834309+hoanghaix86@users.noreply.github.com>
2024-04-03 00:37:48 +0700

    feat: warp

---
+++ in-warp.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+sudo apt-get update
+
+sudo apt-get install -y curl 
+
+url="https://app.warp.dev/download?package=deb"
+
+curl -s -L $url --output warp-terminal.deb
+
+sudo dpkg -i warp-terminal.deb
