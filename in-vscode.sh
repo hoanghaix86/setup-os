b48b022fa94af2f8c93ef634b1cf1b41d1aa5e3c
hoanghaix86 <97834309+hoanghaix86@users.noreply.github.com>
2024-04-03 02:02:04 +0700

    fix: bypass yes

--- in-vscode.sh
+++ in-vscode.sh
@@ -1,19 +1,17 @@
 #!/bin/sh
 
-sudo apt-get install wget gpg
+sudo apt-get install -y wget gpg apt-transport-https
 
 wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
 
 sudo install -D -o root -g root -m 644 packages.microsoft.gpg /etc/apt/keyrings/packages.microsoft.gpg
 
-cat > /etc/apt/sources.list.d/vscode.list <<EOF
+cat <<EOF | sudo tee /etc/apt/sources.list.d/vscode.list > /dev/null
 deb [arch=amd64,arm64,armhf signed-by=/etc/apt/keyrings/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main
 EOF
 
 rm -f packages.microsoft.gpg
 
-sudo apt install apt-transport-https
+sudo apt-get update
 
-sudo apt update
-
-sudo apt install code # or code-insiders
+sudo apt-get install -y code # or code-insiders
