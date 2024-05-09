9fc44bd51342ac92be9b54a8052fd60e3ed9dbc2
hoanghaix86 <97834309+hoanghaix86@users.noreply.github.com>
2024-05-09 09:20:37 +0700

    feat: script install vagrant

--- in-vagrant.sh
+++ in-vagrant.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
-wget -O- https://apt.releases.hashicorp.com/gpg | sudo gpg --dearmor -o /usr/share/keyrings/hashicorp-archive-keyring.gpg --batch --yes
-echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/hashicorp.list
-sudo apt update && sudo apt install vagrant -y
+sudo apt update
+
+sudo apt install wget unzip -y
+
+wget https://releases.hashicorp.com/vagrant/2.4.1/vagrant_2.4.1_linux_amd64.zip
+
+unzip vagrant_2.4.1_linux_amd64.zip
+
+rm vagrant_2.4.1_linux_amd64.zip
+
+sudo mv vagrant /usr/local/bin/vagrant
+
+sudo chown $USER.$USER /usr/local/bin/vagrant
