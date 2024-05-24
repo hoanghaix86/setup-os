cda01c8766d46e00b12461af9d6ec889a1d4b63c
hoanghaix86 <97834309+hoanghaix86@users.noreply.github.com>
2024-05-24 15:03:41 +0700

    feat: jetbrain

---
+++ in-jetbrains-toolbox.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+sudo apt update
+
+sudo apt install curl jq libfuse2 -y
+
+LINK_DOWNLOAD=$(curl -s 'https://data.services.jetbrains.com/products?code=TBA&release.type=release' | jq -r '.[0].releases[0].downloads.linux.link')
+
+FILE_NAME=$(jq -n -r --arg str $LINK_DOWNLOAD '$str | split("/") | .[-1]')
+
+curl -s -L -O $LINK_DOWNLOAD
+
+tar -xf $FILE_NAME
+
+rm $FILE_NAME
+
+sudo mv jetbrains-toolbox* /opt/jetbrains-toolbox
+
+/opt/jetbrains-toolbox/jetbrains-toolbox
