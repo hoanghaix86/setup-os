4a73b0a478b3b9178594cafef842a2b88d0fa44e
hoanghaix86 <97834309+hoanghaix86@users.noreply.github.com>
2024-07-05 22:14:25 +0700

    feat: pyenv

---
+++ in-pyenv.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+sudo apt-get update
+sudo apt-get install -y make build-essential libssl-dev \
+    zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm \
+    libncursesw5-dev xz-utils tk-dev libxml2-dev libxmlsec1-dev libffi-dev liblzma-dev
+
+curl https://pyenv.run | bash
+
+tee -a ~/.zshrc > /dev/null <<EOF
+export PYENV_ROOT="\$HOME/.pyenv"
+[[ -d \$PYENV_ROOT/bin ]] && export PATH="\$PYENV_ROOT/bin:\$PATH"
+eval "$(pyenv init -)"
+eval "$(pyenv virtualenv-init -)"
+EOF
