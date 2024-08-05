7998ecd1b17f1fa5a8a6c3656f2a0a064f84a544
hoanghaix86 <97834309+hoanghaix86@users.noreply.github.com>
2024-08-06 00:29:57 +0700

    update pyenv

--- in-pyenv.sh
+++ in-pyenv.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 sudo apt-get update
+
 sudo apt-get install -y make build-essential libssl-dev \
     zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm \
     libncursesw5-dev xz-utils tk-dev libxml2-dev libxmlsec1-dev libffi-dev liblzma-dev
@@ -8,8 +9,15 @@ sudo apt-get install -y make build-essential libssl-dev \
 curl https://pyenv.run | bash
 
 tee -a ~/.zshrc > /dev/null <<EOF
-export PYENV_ROOT="\$HOME/.pyenv"
-[[ -d \$PYENV_ROOT/bin ]] && export PATH="\$PYENV_ROOT/bin:\$PATH"
-eval "$(pyenv init -)"
-eval "$(pyenv virtualenv-init -)"
+# pyenv
+if [ -d "\$HOME/.nvm" ];
+then
+  export PYENV_ROOT="\$HOME/.pyenv"
+  [[ -d \$PYENV_ROOT/bin ]] && export PATH="\$PYENV_ROOT/bin:\$PATH"
+  eval "\$(pyenv init -)"
+  eval "\$(pyenv virtualenv-init -)"
+fi
 EOF
+
+# Use
+# pyenv install 3.12.4
