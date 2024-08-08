255913865697c608b4f8216ffc82b5fb9caf4c5a
hoanghaix86 <97834309+hoanghaix86@users.noreply.github.com>
2024-08-09 03:38:56 +0700

    vagrant

--- in-vagrant.sh
+++ in-vagrant.sh
@@ -1,21 +1,9 @@
 #!/bin/bash
 
-sudo apt update
+wget -O- https://apt.releases.hashicorp.com/gpg | sudo gpg --dearmor -o /usr/share/keyrings/hashicorp-archive-keyring.gpg
+echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/hashicorp.list
+sudo apt update && sudo apt install vagrant -y
 
-sudo apt install wget unzip -y
-
-wget https://releases.hashicorp.com/vagrant/2.4.1/vagrant_2.4.1_linux_amd64.zip
-
-unzip vagrant_2.4.1_linux_amd64.zip
-
-rm vagrant_2.4.1_linux_amd64.zip
-
-sudo mv vagrant /usr/local/bin/vagrant
-
-sudo chown $USER.$USER /usr/local/bin/vagrant
-
-sudo su -
-mkdir /etc/vbox/
-cd /etc/vbox/
-echo '* 0.0.0.0/0 ::/0' > /etc/vbox/networks.conf
-chmod 644 /etc/vbox/networks.conf
+sudo mkdir -p /etc/vbox/
+echo '* 0.0.0.0/0 ::/0' | sudo tee /etc/vbox/networks.conf
+sudo chmod 644 /etc/vbox/networks.conf
